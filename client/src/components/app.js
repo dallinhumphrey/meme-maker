620a0ece93154255fb28e485b055719703d8b3c3
dallinhumphrey <dallinhumphrey@gmail.com>
2020-04-27 11:27:46 -0600

    meme maker

---
+++ client/src/components/app.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Meme from "./meme";
+
+export default function App() {
+  const [memes, setMemes] = useState([]);
+
+  const deleteMeme = (id) => {
+    axios
+      .delete(`https://localhost:5000/delete-meme/${id}`)
+      .then(setMemes(memes.filter((meme) => meme.id !== id)))
+      .catch((err) => console.log("Delete Meme err: ", err));
+  };
+
+  const renderMemes = () => {
+    return memes.map((meme) => {
+      return <Meme key={meme.id} meme={meme} deleteMeme={deleteMeme} />;
+    });
+  };
+
+  const getMemes = () => {
+    fetch("http://localhost:5000/memes")
+      .then((res) => res.json())
+      .then((data) => setMemes(data))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    getMemes();
+  }, []);
+  return <div className="app">{renderMemes()}</div>;
+}
---
+++ client/src/components/meme.js
@@ -0,0 +1,22 @@
+import React from "react";
+const Meme = (props) => {
+  const { id, text, image, favorite } = props.meme;
+  return (
+    <div key={id} className="meme">
+      <div className="img-wrapper">
+        <img className="meme-img" src={image} alt="Meme Here" />
+      </div>
+      <p>{text}</p>
+      {favorite ? (
+        <img
+          src="https://library.kissclipart.com/20180830/fuw/kissclipart-twinkle-little-star-clip-art-clipart-twinkle-twin-0d72b7a5dc286d1e.jpg"
+          alt="star"
+        />
+      ) : null}
+
+      <button onClick={() => props.deleteMeme(id)}>DEL</button>
+      <button>EDIT</button>
+    </div>
+  );
+};
+export default Meme;
